6d724066618fa6797ed962df0e28b3ce920c6669
CC-KEH <CC-KEH@github.com>
2022-09-25 12:15:14 +0530

    Scripts added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    public float ForwardForce = 2000f;
+    public float movementSpeed = 500f;
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.AddForce(0,0,ForwardForce*Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(movementSpeed*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-movementSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public Movement movement;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Obstacle"){
+            movement.enabled = false;
+        } 
+
+    }
+}
